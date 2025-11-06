c452cc901ee14912a70103b885c62399ee2657a9
FelipeFagundesCosta <felipefacosta@hotmail.com>
2025-11-06 17:10:28 -0300

    add uv and .env

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from chat-service-net!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ server/serverTCP.py
@@ -0,0 +1,23 @@
+import socket
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HOST = str(os.getenv("HOST", "127.0.0.1"))
+PORT = int(os.getenv("PORT", 12345))
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Servidor TCP escutando em {HOST}:{PORT}...")
+    conn, addr = s.accept()
+    with conn:
+        print(f"Conectado por {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(f"Recebido: {data.decode()}")
+            data2 = b'Ola, Cliente!'
+            conn.sendall(data2)
