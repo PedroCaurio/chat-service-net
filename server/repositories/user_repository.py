f007f04c8b0ed33b165275e35c56c5f4535b9978
mathyc0de <mateushackrs7@gmail.com>
2025-11-07 13:22:14 -0300

    feat: CRUD operations

--- server/repositories/user_repository.py
+++ server/repositories/user_repository.py
@@ -4,21 +4,40 @@ from database import db
 
 user_db = db.table("users")
 
+def id_query(user_id: str): return Query().user_id == user_id
+
 class UserRepository:
     
+    # CRUD Operations
+
     @staticmethod
-    def add_user(user: User) -> bool:
+    def add_user(user: User) -> bool:   # CREATE
         if (UserRepository.user_exists(user.username)): return False
         user_db.insert(user.to_dict())
         return True
     
     @staticmethod
-    def get_user_by_id(user_id: str) -> User | None:
-        result = user_db.get(doc_id=user_id)
-        result["id"] = user_id
+    def get_user_by_id(user_id: str) -> User | None:   # READ
+        result = user_db.get(id_query(user_id))
         if result:
             return User.from_dict(result)
         return None
+    
+    @staticmethod
+    def delete_user_by_id(user_id: str) -> bool:   # DELETE
+        return len(user_db.remove(id_query(user_id))) > 0
+
+    @staticmethod
+    def update_user(user: User) -> bool:   # UPDATE
+        User = Query()
+        if not user_db.contains(id_query(user.user_id)) or user_db.contains(User.username == user.username and User.user_id != user.user_id):
+            return False
+        user_db.update(user.to_dict(), cond=id_query(user.user_id))
+        return True
+
+
+
+    # Utils
     
     @staticmethod
     def get_all_users() -> list[User]:
@@ -29,7 +48,3 @@ class UserRepository:
     def user_exists(username: str) -> bool:
         result = user_db.get(Query().username == username)
         return result is not None
-
-    @staticmethod
-    def delete_user_by_id(user_id: str) -> None:
-        user_db.remove(doc_ids=[user_id])
--- server/services/action.py
+++ server/services/action.py
@@ -5,17 +5,27 @@ from datetime import datetime
 
 from registry import register_command
 
+# User Actions
+
 @register_command("action", "create_user")
 def create_user(username: str, password: str):
     return UserRepository.add_user(
         User(username=username, password=password, user_id=str(uuid.uuid4()))
     )
 
+@register_command("action", "get_user")
+def get_user(user_id: str) -> User | None:
+    return UserRepository.get_user_by_id(user_id)
 
-@register_command("action", "get_all_users")
-def get_all_users() -> list[User]:
-    return UserRepository.get_all_users()
+@register_command("action", "update_user")
+def update_user(new_user: dict) -> bool:
+    return UserRepository.update_user(User.from_dict(new_user))
 
+@register_command("action", "delete_user")
+def delete_user(user_id: str) -> bool:
+    return UserRepository.delete_user_by_id(user_id)
+
+# User - Group Actions
 
 @register_command("action", "create_group") 
 def create_group(admin: str, name: str, users: list[str]) -> bool:
@@ -28,3 +38,35 @@ def create_group(admin: str, name: str, users: list[str]) -> bool:
             group_id=str(uuid.uuid4())
         )
     )
+
+@register_command("action", "get_group")
+def get_group(group_id: str) -> Group | None:
+    return GroupRepository.get_group_by_id(group_id)
+
+@register_command("action", "update_group_name")
+def update_group_name(group: dict) -> bool:
+    return GroupRepository.update_group_name(Group.from_dict(group))
+
+@register_command("action", "delete_group")
+def delete_group(group_id: str) -> bool:
+    return GroupRepository.delete_group_by_id(group_id)
+
+@register_command("action", "add_member")
+def add_member(group_id: str, user_id: str) -> bool:
+    return GroupRepository.add_user_to_group(group_id, user_id)
+
+@register_command("action", "kick_member")
+def kick_member(group_id: str, user_id: str) -> bool:
+    return GroupRepository.remove_user_from_group(group_id, user_id)
+
+
+# Misc/Test Actions
+
+
+@register_command("action", "get_all_users")
+def get_all_users() -> list[User]:
+    return UserRepository.get_all_users()
+
+
+def sync():
+    ... # Placeholder for sync logic
